3e0045256c85695e2a93efb8d64955fd42b61675
koningcool <koningcool@imuon.com>
2020-05-13 17:03:13 +0200

    working with 1 problem that i can fix

--- src/koningcool/loadallworlds/Main.php
+++ src/koningcool/loadallworlds/Main.php
@@ -1,9 +1,6 @@
 <?php
-
 declare(strict_types=1);
-
 namespace koningcool\loadallworlds;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
@@ -15,7 +12,6 @@ use pocketmine\plugin\Plugin;
 
 
 interface PluginIdentifiableCommand{
-
 	/**
 	 * @return Plugin
 	 */
@@ -23,37 +19,45 @@ interface PluginIdentifiableCommand{
 }
 
 
-
 class Main extends PluginBase{
+     /** @var Config */
+    public $myConfig;
     public $cfg;  
 
     private function loadWorlds() : void{
         foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName){
-            if($this->getServer()->loadLevel($levelName));
-                      
+            if($this->getServer()->loadLevel($levelName));           
         }    
     }    
-
     public function onLoad() : void{
             $this->getLogger()->info(TextFormat::DARK_BLUE . "LoadAllWorlds Loaded!");}
 
     public function onEnable() : void{
         $this->getLogger()->info(TextFormat::DARK_GREEN . "LoadAllWorlds Enabled!");
+        if (file_exists($this->getDataFolder() . "config.yml")){
+            $this->getLogger()->info(TextFormat::DARK_GREEN . $this->getConfig()->get("load-on-startup"));
+            if($this->getConfig()->get("load-on-startup") === true){
 
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
         $this->cfg = $this->getConfig()->getAll();
-        if($this->cfg->get("load-on-startup") === true){
+        if($this->cfg["load-on-startup"] === true){
             $this->getLogger()->info(TextFormat::DARK_GREEN . "true");
                 $this->loadWorlds(); 
-                
-            
+            }  
         }
- }
+        else{
+            @mkdir($this->getDataFolder());
+            $this->saveResource("config.yml");
+            $this->myConfig = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+            $this->saveDefaultConfig();
+            $this->getConfig()->get("load-on-startup");
+
+        }
+    }
 
     public function onDisable() : void{
         $this->getLogger()->info(TextFormat::DARK_RED . "LoadAllWorlds Disabled!");}
-
         
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         switch($command->getName()){
