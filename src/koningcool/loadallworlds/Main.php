aa7edaab152c14d3f9d171786bea2b106c72cc66
koningcool <koningcool@imuon.com>
2020-05-15 18:48:10 +0200

    working for me!

--- src/koningcool/loadallworlds/Main.php
+++ src/koningcool/loadallworlds/Main.php
@@ -20,7 +20,9 @@ interface PluginIdentifiableCommand{
 
 
 class Main extends PluginBase{
-       
+     /** @var Config */
+    public $myConfig;
+    public $cfg;  
 
     private function loadWorlds() : void{
         foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName){
@@ -28,23 +30,35 @@ class Main extends PluginBase{
         }    
     }    
     public function onLoad() : void{
-            //$this->getLogger()->info(TextFormat::DARK_BLUE . "LoadAllWorlds Loaded!");
-    }
+            $this->getLogger()->info(TextFormat::DARK_BLUE . "LoadAllWorlds Loaded!");}
 
     public function onEnable() : void{
-        //$this->getLogger()->info(TextFormat::DARK_GREEN . "LoadAllWorlds Enabled!");
-     
-    }  
-    
-        
+        $this->getLogger()->info(TextFormat::DARK_GREEN . "LoadAllWorlds Enabled!");
+        if (file_exists($this->getDataFolder() . "config.yml")){
+            $this->getLogger()->info(TextFormat::DARK_GREEN . $this->getConfig()->get("load-on-startup"));
+            if($this->getConfig()->get("load-on-startup") === true){
 
-        
-        
-    
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->cfg = $this->getConfig()->getAll();
+        if($this->cfg["load-on-startup"] === true){
+            $this->getLogger()->info(TextFormat::DARK_GREEN . "true");
+                $this->loadWorlds(); 
+            }  
+        }
+        else{
+            @mkdir($this->getDataFolder());
+            $this->saveResource("config.yml");
+            $this->myConfig = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+            $this->saveDefaultConfig();
+            $this->getConfig()->get("load-on-startup");
+
+        }
+        }
+    }
 
     public function onDisable() : void{
-        //$this->getLogger()->info(TextFormat::DARK_RED . "LoadAllWorlds Disabled!");
-    }
+        $this->getLogger()->info(TextFormat::DARK_RED . "LoadAllWorlds Disabled!");}
         
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         switch($command->getName()){
