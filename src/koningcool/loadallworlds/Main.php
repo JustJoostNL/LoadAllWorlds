70de37e07281906b78e90b6ed70516fd993da21d
koningcool <koningcool@imuon.com>
2020-05-16 11:59:43 +0200

    pocket pupdate 2

--- src/koningcool/loadallworlds/Main.php
+++ src/koningcool/loadallworlds/Main.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace koningcool\loadallworlds;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
@@ -10,47 +11,45 @@ use function array_diff;
 use function scandir;
 use pocketmine\plugin\Plugin;
 
-
-interface PluginIdentifiableCommand{
-	/**
-	 * @return Plugin
-	 */
-	public function getPlugin() : Plugin;
+interface PluginIdentifiableCommand
+{
+    /**
+     * @return Plugin
+     */
+    public function getPlugin() : Plugin;
 }
 
 
-class Main extends PluginBase{
-       
-
-    private function loadWorlds() : void{
-        foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName){
-            if($this->getServer()->loadLevel($levelName));           
-        }    
-    }    
-    public function onLoad() : void{
-            //$this->getLogger()->info(TextFormat::DARK_BLUE . "LoadAllWorlds Loaded!");
+class Main extends PluginBase
+{
+    private function loadWorlds() : void
+    {
+        foreach (array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName) {
+            if ($this->getServer()->loadLevel($levelName));
+        }
+    }
+    
+    public function onLoad() : void
+    {
+        //$this->getLogger()->info(TextFormat::DARK_BLUE . "LoadAllWorlds Loaded!");
     }
 
-    public function onEnable() : void{
+    public function onEnable() : void
+    {
         //$this->getLogger()->info(TextFormat::DARK_GREEN . "LoadAllWorlds Enabled!");
-     
-    }  
-    
-        
+    }
 
-        
-        
-    
-
-    public function onDisable() : void{
+    public function onDisable() : void
+    {
         //$this->getLogger()->info(TextFormat::DARK_RED . "LoadAllWorlds Disabled!");
     }
         
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch($command->getName()){
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
+    {
+        switch ($command->getName()) {
             case "loadall":
-                $this->loadWorlds();              
+                $this->loadWorlds();
         }
-        return true;   
-	}    
+        return true;
+    }
 }
